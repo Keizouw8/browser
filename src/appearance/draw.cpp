8f52f252eebd78b38dcd6c3df6d2caef0e58cf18
Keizou Wang <keizouw@proton.me>
2025-12-07 16:23:05 -0800

    close tab

--- src/appearance/draw.cpp
+++ src/appearance/draw.cpp
@@ -1,19 +1,22 @@
 #include "../browser.h"
 #include "SFML/Graphics/Color.hpp"
 #include <vector>
+#include <iostream>
 
 void Browser::drawTabs(){
 	int i = 0;
+	Workspace* ws = workspaces[activeWorkspace];
 
-	for (; i < workspaces[activeWorkspace]->pins.size(); i++){
-		sf::Text text(workspaces[activeWorkspace]->activeTab == i ? styles.fonts.bold_italic : styles.fonts.bold, std::format("{} {}", i + 1, workspaces[activeWorkspace]->pins[i]->title), 14);
-		text.setFillColor(workspaces[activeWorkspace]->pins[i]->loaded ? styles.colors.primary : styles.colors.tertiary);
+	for (; i < ws->pins.size(); i++){
+		if(ws->pins[i]->loaded) std::cout << "loaded!" << std::endl;
+		sf::Text text(ws->activeTab == i ? styles.fonts.bold_italic : styles.fonts.bold, std::format("{} {}", i + 1, ws->pins[i]->title), 14);
+		text.setFillColor(ws->pins[i]->loaded ? styles.colors.primary : styles.colors.tertiary);
 		text.setPosition({ 10, 5 + 20.f * i });
 		window.draw(text);
 	}
 
-	for (int j = 0; j < workspaces[activeWorkspace]->tabs.size(); j++, i++){
-		sf::Text text(workspaces[activeWorkspace]->activeTab == i ? styles.fonts.italic : styles.fonts.regular, std::format("{} {}", i + 1, workspaces[activeWorkspace]->tabs[j]->title), 14);
+	for (int j = 0; j < ws->tabs.size(); j++, i++){
+		sf::Text text(ws->activeTab == i ? styles.fonts.italic : styles.fonts.regular, std::format("{} {}", i + 1, ws->tabs[j]->title), 14);
 		text.setFillColor(styles.colors.primary);
 		text.setPosition({ 10, 5 + 20.f * i });
 		window.draw(text);
@@ -36,7 +39,7 @@ void Browser::drawWorkspaces(){
 	for (int i = 0; i < ws_length; i++){
 		sf::Text text(styles.fonts.bold, std::format("{}", i + 1), 14);
 		text.setFillColor(activeWorkspace == i ? styles.colors.primary : styles.colors.tertiary);
-		text.setPosition({ 0.5f*(styles.sidebarWidth-24*ws_length)+24*i+8, viewPort.size.y - 22 });
+		text.setPosition({ 0.5f*(styles.sidebarWidth - 24*ws_length) + 24*i + 8, viewPort.size.y - 22 });
 		window.draw(text);
 	}
 }
--- src/events/tabManagement.cpp
+++ src/events/tabManagement.cpp
@@ -7,5 +7,20 @@ void Browser::newTab(){
 	lastTab();
 }
 
-void Browser::closeTab(){}
+void Browser::closeTab(){
+	Workspace* ws = workspaces[activeWorkspace];
+	if(ws->activeTab < 0) return;
+	if(ws->activeTab < ws->pins.size()){
+		ws->pins[ws->activeTab]->loaded = false;
+		for(; ws->activeTab < ws->pins.size(); ws->activeTab++) if(ws->pins[ws->activeTab]->loaded) break;
+	}else{
+		int i = ws->activeTab - ws->pins.size();
+		ws->tabs.erase(ws->tabs.begin() + i);
+	}
+	if(ws->activeTab < ws->size()) return;
+	ws->activeTab = ws->size() - 1;
+	if(ws->activeTab >= ws->pins.size()) return;
+	for(ws->activeTab--; ws->activeTab >= 0; ws->activeTab--);
+}
+
 void Browser::refresh(){}
